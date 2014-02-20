51e10174f926cc1fb60ddb5117d413e5467ad094
jah1138 <jah@malcontent.info>
2014-02-19 17:08:07 -0800

    Final homework harris04

---
+++ harrisa04/src/edu/washington/ext/Binding.java
@@ -0,0 +1,16 @@
+package edu.washington.ext;
+
+/**
+ * A selection of different bindings for a book.
+ *
+ * @author Alex Harris
+ * @version February 12, 2014
+ */
+public enum Binding {
+    HARDCOVER,
+    PAPERBACK,
+    EPUB,
+    MOBI,
+    APK
+}
+
